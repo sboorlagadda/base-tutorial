e5cee8f52e6491463502bbb8de0ad0038a5325e4
Sai Boorlagadda <sai.boorlagadda@gmail.com>
2018-06-20 05:03:41 -0700

    using await

--- README.md
+++ README.md
@@ -1,9 +1,53 @@
-#Base Platform Tutorial
+# Base Platform Tutorial
 
-This reference application demonstrates how to use BASE API using [BASE JS LIBRARY](https://github.com/bitclave/base-client-js). The application asks user to enter a passphrase to create his private & public keys used to interact with the platform. Either passphrase or private key is never sent to BASE platform. Once user has his keys, he can create his account and use his private key to encrypt his data and save it in the platform.
+This reference application demonstrates how to use BASE API using [BASE JS LIBRARY](https://github.com/bitclave/base-client-js). 
 
+The application lets user to choose a passphrase to create his private & public key pair and use private key to encrypt his data and save to BASE. Saved user retrieve his data any time and decrypt it using his private key. Either passphrase or private key is never sent to BASE platform.
 
-Steps to run this tutorial:
+# How to use Base Platform
+1) Initialise Base
+```
+const base = new Base("https://base-node-staging.herokuapp.com", 'localhost', '', '');    
+```
 
-1) npm i
-2) npm run start
+2) Choose a passphrase and crate a keypair
+```
+base.createKeyPairHelper('').createKeyPair("some passphrase").then(keyPair => {
+    console.log("PublicKey:" + keyPair.publicKey);
+    console.log("PrivateKey:" + keyPair.privateKey);     
+});
+```
+
+3) Create account or check for existence
+```
+let account;
+try {
+    account = await base.accountManager.checkAccount(passphrase, "somemessage");
+} catch(e) {
+    account = await base.accountManager.registration(passphrase, "somemessage");
+}
+```
+
+4) Save & Retrieve your data
+```
+//Save
+let data = new Map();
+data.set("email", "im@host.com");
+let encryptedData = await base.profileManager.updateData(data);
+
+//Read
+let decryptedData = await base.profileManager.getData();
+console.log(decryptedData.get("email"));
+```
+
+# Steps to run this tutorial
+
+```
+$ node --version
+v8.11.2
+$ npm --version
+6.1.0
+
+$ npm i
+$ npm run start
+```
--- src/index.js
+++ src/index.js
@@ -1,44 +1,59 @@
 'use strict';
 
 import Base from "./lib/Bitclave-Base";
-import * as os from 'os';
 
-function demo() {
+//required for babel to polyfill regeneratorRuntime
+require("babel-polyfill");
+
+process.on('unhandledRejection', (reason, p) => {
+    console.log('Unhandled Rejection at: Promise', p, 'reason:', reason);
+});
+
+(function() {
+    //Define a passphrase
     const passphrase = 'some passphrase';
     
+    //Initialize Base
     const base = new Base("https://base-node-staging.herokuapp.com", 'localhost', '', '');
     base.changeStrategy('POSTGRES');
-    base.createKeyPairHelper('').createKeyPair(passphrase).then(keyPair => {
+    
+    //Create a KeyPair
+    base.createKeyPairHelper('').createKeyPair(passphrase).then(async keyPair => {
+
+        console.log("\nCreated a keypair for the passphrase: " + passphrase);
         console.log("PublicKey:" + keyPair.publicKey);
         console.log("PrivateKey:" + keyPair.privateKey);
-        //check for existence or create a new account using registration
-        base.accountManager.checkAccount(passphrase, "somemessage").then(account => {
-            console.log("\nAccount created:" + JSON.stringify(account));
-            let data = new Map();
-            data.set("firstname", "John");
-            data.set("lastname", "Doe");
-            data.set("email", "john.doe@gmail.com");
-            data.set("city", "NewYork");
-            base.profileManager.updateData(data).then(encryptedData => {
-                console.log("\nUser data is encrypted and saved to Base.");
-                for (var [key, value] of encryptedData.entries()) {
-                    console.log("Key:" + key + ", Encrypted Value:" + value);
-                }
-                base.profileManager.getData().then(decryptedData => {
-                    console.log("\nUser data is retrieved from Base and decrypted.");
-                    for (var [key, value] of decryptedData.entries()) {
-                        console.log("Key:" + key + ", Decrypted Value:" + value);
-                    }
-                }).catch(e => {
-                    console.log(e);
-                });
-            }).catch(e => {
-                console.log(e);
-            });
-        }).catch(e => {
-            console.log(e);
-        });
-    });
-}
 
-demo();
+        //Check for existence or create a new account
+        let account;
+        try {
+           console.log("\nChecking if account already exists.");
+           account = await base.accountManager.checkAccount(passphrase, "somemessage");
+           console.log("Account already exists: " + JSON.stringify(account));
+        } catch(e) {
+           console.log("\nAccount doesn't exist, Creating a new one.");
+           account = await base.accountManager.registration(passphrase, "somemessage");
+           console.log("Account created:" + JSON.stringify(account));
+        }
+
+        let data = new Map();
+        data.set("firstname", "John");
+        data.set("lastname", "Doe");
+        data.set("email", "john.doe@gmail.com");
+        data.set("city", "NewYork");
+
+        // Save encrypted data to Base
+        let encryptedData = await base.profileManager.updateData(data);
+        console.log("\nUser data is encrypted and saved to Base.");
+        for (var [key, value] of encryptedData.entries()) {
+            console.log("Key:" + key + ", Encrypted Value:" + value);
+        }
+
+        // Read saved data and decrypt
+        let decryptedData = await base.profileManager.getData();
+        console.log("\nUser data is retrieved from Base and decrypted.");
+        for (var [key, value] of decryptedData.entries()) {
+            console.log("Key:" + key + ", Decrypted Value:" + value);
+        }
+    });
+})();
