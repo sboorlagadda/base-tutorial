26a5c1029f76ad561ad207eda240fb2181efb1db
Sai Boorlagadda <sai.boorlagadda@gmail.com>
2018-06-20 00:17:51 -0700

    invoked changeStrategy on Base

--- src/index.js
+++ src/index.js
@@ -7,17 +7,28 @@ function demo() {
     const passphrase = 'some passphrase';
     
     const base = new Base("https://base-node-staging.herokuapp.com", 'localhost', '', '');
+    base.changeStrategy('POSTGRES');
     base.createKeyPairHelper('').createKeyPair(passphrase).then(keyPair => {
         console.log("PublicKey:" + keyPair.publicKey);
         console.log("PrivateKey:" + keyPair.privateKey);
         //check for existence or create a new account using registration
         base.accountManager.checkAccount(passphrase, "somemessage").then(account => {
-            console.log("Account created:" + JSON.stringify(account));
-            let myMap = new Map();
-            myMap.set("email", "im@host.com");
-            base.profileManager.updateData(myMap).then(encryptedData => {
-                base.profileManager.getData().then(data => {
-                    console.log("user data:" + data);
+            console.log("\nAccount created:" + JSON.stringify(account));
+            let data = new Map();
+            data.set("firstname", "John");
+            data.set("lastname", "Doe");
+            data.set("email", "john.doe@gmail.com");
+            data.set("city", "NewYork");
+            base.profileManager.updateData(data).then(encryptedData => {
+                console.log("\nUser data is encrypted and saved to Base.");
+                for (var [key, value] of encryptedData.entries()) {
+                    console.log("Key:" + key + ", Encrypted Value:" + value);
+                }
+                base.profileManager.getData().then(decryptedData => {
+                    console.log("\nUser data is retrieved from Base and decrypted.");
+                    for (var [key, value] of decryptedData.entries()) {
+                        console.log("Key:" + key + ", Decrypted Value:" + value);
+                    }
                 }).catch(e => {
                     console.log(e);
                 });
